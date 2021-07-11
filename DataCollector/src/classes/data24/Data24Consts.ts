374a254e45de1e40a7ec72be9da14034f6613ab1
kingz <eyedroot@gmail.com>
2021-07-11 20:52:34 +0900

    fix: update types.ts

--- DataCollector/src/classes/data24/Data24Consts.ts
+++
@@ -1,2 +0,0 @@
-export const DATA24_HOST: string = 'http://apis.data.go.kr'
-export const DATA24_MDCIN_URI: string = '/1471000/MdcinExaathrService01/getMdcinExaathrList01'
--- DataCollector/src/index.ts
+++ DataCollector/src/index.ts
@@ -1,5 +1,4 @@
 import 'dotenv/config'
 import Mdcin from './classes/collector/Mdcin'
 
-
 const mdcinInst = new Mdcin(process.env.MDCIN_ENCODING_KEY!)
