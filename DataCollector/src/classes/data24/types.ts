6b6b022a2b2761d56ded087620066e770c5826ac
kingz <eyedroot@gmail.com>
2021-07-11 20:59:13 +0900

    update: environment

--- DataCollector/src/classes/data24/types.ts
+++
@@ -1,2 +0,0 @@
-export const DATA24_HOST = 'http://apis.data.go.kr'
-export const DATA24_MDCIN_URI = '/1471000/MdcinExaathrService01/getMdcinExaathrList01'
---
+++ DataCollector/src/types.ts
@@ -0,0 +1,4 @@
+namespace Data24 {
+  export const API_HOST = 'http://apis.data.go.kr'
+  export const API_MDCIN_URI = '/1471000/MdcinExaathrService01/getMdcinExaathrList01'
+}
