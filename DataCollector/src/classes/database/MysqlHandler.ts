1bfed226d740224d40673b7c2e6c6f13f29c1187
kingz <eyedroot@gmail.com>
2021-07-18 18:49:18 +0900

    update: MysqlHandler

--- DataCollector/src/classes/database/MysqlHandler.ts
+++ DataCollector/src/classes/database/MysqlHandler.ts
@@ -2,42 +2,53 @@ import mysql from 'mysql2/promise';
 
 export default class MysqlHandler<T>
 {
-  private static connection: mysql.Connection | undefined = undefined;
+  private static pool: mysql.Pool;
   protected tableName: string = '';
   protected primaryKey: string = 'id';
   protected fields: T[] = [];
 
   public constructor()
   {
-    this.connect();
+    MysqlHandler.connect();
   }
 
-  private async connect()
+  private static connect(): void
   {
     // <Connection Pool>
     // https://www.npmjs.com/package/mysql2#using-connection-pools
-    // const connection = mysql.createPool({
-    //   "host": process.env.MYSQL_HOST,
-    //   "user": process.env.MYSQL_USER,
-    //   "password": process.env.MYSQL_PASSWORD,
-    //   "database": process.env.MYSQL_DB_NAME,
-    //   "waitForConnections": true,
-    //   "connectTimeout": 10,
-    //   "queueLimit": 0,
-    // });
-
-    // <Connection Promise>
-    // https://www.npmjs.com/package/mysql2#using-promise-wrapper
-    MysqlHandler.connection = MysqlHandler.connection || await mysql.createConnection({
+    MysqlHandler.pool = mysql.createPool({
       "host": process.env.MYSQL_HOST,
       "user": process.env.MYSQL_USER,
       "password": process.env.MYSQL_PASSWORD,
       "database": process.env.MYSQL_DB_NAME,
+      "waitForConnections": true,
+      "connectTimeout": 10,
+      "queueLimit": 0,
     });
+
+    // <Connection Promise>
+    // https://www.npmjs.com/package/mysql2#using-promise-wrapper
+    // MysqlHandler.connection = MysqlHandler.connection || await mysql.createConnection({
+    //   "host": process.env.MYSQL_HOST,
+    //   "user": process.env.MYSQL_USER,
+    //   "password": process.env.MYSQL_PASSWORD,
+    //   "database": process.env.MYSQL_DB_NAME,
+    // });
   }
 
-  public getConnection(): mysql.Connection | undefined
+  public static async getConnection(): Promise<mysql.PoolConnection>
   {
-    return MysqlHandler.connection;
+    if (!MysqlHandler.pool) {
+      MysqlHandler.connect();
+    }
+
+    const connection = await MysqlHandler.pool.getConnection();
+
+    return connection;
+  }
+
+  public inserts(massive: Array<T>)
+  {
+
   }
 }
