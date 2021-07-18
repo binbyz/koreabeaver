52a36400ee5df36e57cc4ac3b92716cf7f738098
kingz <eyedroot@gmail.com>
2021-07-18 17:55:03 +0900

    update: MysqlHandler

--- DataCollector/src/classes/database/MysqlHandler.ts
+++ DataCollector/src/classes/database/MysqlHandler.ts
@@ -1,34 +1,43 @@
-import mysql from 'mysql2';
+import mysql from 'mysql2/promise';
 
 export default class MysqlHandler<T>
 {
-  private connection: mysql.Pool;
+  private static connection: mysql.Connection | undefined = undefined;
   protected tableName: string = '';
   protected primaryKey: string = 'id';
   protected fields: T[] = [];
 
   public constructor()
   {
+    this.connect();
+  }
+
+  private async connect()
+  {
     // <Connection Pool>
     // https://www.npmjs.com/package/mysql2#using-connection-pools
-    this.connection = mysql.createPool({
+    // const connection = mysql.createPool({
+    //   "host": process.env.MYSQL_HOST,
+    //   "user": process.env.MYSQL_USER,
+    //   "password": process.env.MYSQL_PASSWORD,
+    //   "database": process.env.MYSQL_DB_NAME,
+    //   "waitForConnections": true,
+    //   "connectTimeout": 10,
+    //   "queueLimit": 0,
+    // });
+
+    // <Connection Promise>
+    // https://www.npmjs.com/package/mysql2#using-promise-wrapper
+    MysqlHandler.connection = MysqlHandler.connection || await mysql.createConnection({
       "host": process.env.MYSQL_HOST,
       "user": process.env.MYSQL_USER,
       "password": process.env.MYSQL_PASSWORD,
       "database": process.env.MYSQL_DB_NAME,
-      "waitForConnections": true,
-      "connectTimeout": 10,
-      "queueLimit": 0,
     });
   }
 
-  public getConnection(): mysql.Pool
+  public getConnection(): mysql.Connection | undefined
   {
-    return this.connection;
-  }
-
-  public getTableName(): string
-  {
-    return this.tableName;
+    return MysqlHandler.connection;
   }
 }
