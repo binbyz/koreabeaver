f4f7916d6fb7df7873f131b188e34f658a29bbd6
kingz <eyedroot@gmail.com>
2021-07-18 16:29:51 +0900

    update: MdcinModel

--- DataCollector/src/classes/models/MdcinModel.ts
+++ DataCollector/src/classes/models/MdcinModel.ts
@@ -1,6 +1,20 @@
 import Data24Model from './Data24Model';
 import { Data24 } from '../../types';
 
+interface ItemRow
+{
+  ADM_DISPS_SEQ: number; // 행정처분일련번호
+  ENTP_NAME: string; // 업소명
+  ADDR: string | null; // 업소소재지
+  ENTP_NO: number; // 업허가번호
+  ITEM_NAME: string; // 제품명
+  BEF_APPLY_LAW: string | null; // 위반법명
+  EXPOSE_CONT: string; // 위반내용
+  ADM_DISPS_NAME: string; // 행정처분명
+  LAST_SETTLE_DATE: number | null; // 행정처분일자 (최종확정일자)
+  DISPS_TERM_DATE: string; // 행정처분기간
+}
+
 export default class MdcinModel extends Data24Model implements Data24.ModelHandler
 {
   public constructor()
@@ -17,7 +31,11 @@ export default class MdcinModel extends Data24Model implements Data24.ModelHandl
       return false;
     }
 
-    const items = this.content.response.body.items;
+    const items: Array<ItemRow> = this.content.response.body.items.item;
+
+    items.forEach((value) => {
+      console.log(value);
+    });
 
     return true;
   }
