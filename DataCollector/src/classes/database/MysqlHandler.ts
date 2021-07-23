c414e8d5e4b7fdf4006c5c6e0dcf09e8543cb2c4
kingz <eyedroot@gmail.com>
2021-07-24 00:42:09 +0900

    update: MysqlHandler

--- DataCollector/src/classes/database/MysqlHandler.ts
+++ DataCollector/src/classes/database/MysqlHandler.ts
@@ -5,12 +5,27 @@ import { isUndefined } from '../../lib/slim';
 
 type QueryType = 'insert' | 'select' | 'update' | 'delete' | 'upsert';
 
+/**
+ * `boolean` is EXISTS
+ */
+type whereValueCondition = string | number | boolean;
+
+const CONNECTION_TIMEOUT = 10;
+const DEFAULT_LIMIT = 2000;
+
 export default class MysqlHandler<T extends IndexSignature>
 {
   private static pool: mysql.Pool;
+
+  private whereCondition: Map<string, whereValueCondition> = new Map<string, whereValueCondition>();
+  private fields: string[] = [];
+  private _offset: number = 0;
+  private _limit: number = DEFAULT_LIMIT;
+
+  // TODO implements `database name` property
+
   protected tableName: string = '';
   protected primaryKey: string = 'id';
-  protected fields: T[] = [];
 
   public constructor()
   {
@@ -27,7 +42,7 @@ export default class MysqlHandler<T extends IndexSignature>
       "password": process.env.MYSQL_PASSWORD,
       "database": process.env.MYSQL_DB_NAME,
       "waitForConnections": true,
-      "connectTimeout": 10,
+      "connectTimeout": CONNECTION_TIMEOUT,
       "queueLimit": 0,
     });
 
@@ -59,7 +74,7 @@ export default class MysqlHandler<T extends IndexSignature>
   public async inserts(massive: Array<T>)
   {
     const connection = await MysqlHandler.getConnection();
-    const query = this.makeQuery('insert', massive);
+    const query = this.makeInsertQuery('insert', massive);
 
     try {
       await connection.beginTransaction();
@@ -85,6 +100,76 @@ export default class MysqlHandler<T extends IndexSignature>
   }
 
   /**
+   * select
+   * @param fields string | string[]
+   * @returns
+   */
+  public select(fields: string | string[]): this
+  {
+    if (!Array.isArray(fields)) { // is string
+      fields = fields.split(',').map(v => v.trim());
+    }
+
+    this.fields = fields;
+
+    return this;
+  }
+
+  /**
+   * where
+   * @param key string
+   * @param condition whereValueCondition
+   * @returns this
+   */
+  public where(key: string, condition: whereValueCondition): this
+  {
+    this.whereCondition.set(key, condition);
+
+    return this;
+  }
+
+  /**
+   * set offset
+   * @param offset number
+   * @returns
+   */
+  public offset(offset: number): this
+  {
+    this._offset = Math.abs(offset);
+
+    return this;
+  }
+
+  /**
+   * set limit
+   * @param limit number
+   * @returns
+   */
+  public limit(limit: number): this
+  {
+    this._limit = Math.abs(limit);
+
+    return this;
+  }
+
+  /**
+   * 모든 `whereCondition`을 초기화합니다.
+   * @returns this
+   */
+  public clear(): this
+  {
+    this.whereCondition.clear();
+
+    return this;
+  }
+
+  public get(): void
+  {
+    const query: string = this.makeSelectQuery();
+    console.log(query);
+  }
+
+  /**
    * Upserts
    * @param massive
    * @param key
@@ -93,7 +178,7 @@ export default class MysqlHandler<T extends IndexSignature>
   public async upserts(massive: Array<T>, key: string, options: Array<string>)
   {
     const connection = await MysqlHandler.getConnection();
-    const query = this.makeQuery('upsert', massive, key, options);
+    const query = this.makeInsertQuery('upsert', massive, key, options);
 
     try {
       await connection.beginTransaction();
@@ -123,14 +208,14 @@ export default class MysqlHandler<T extends IndexSignature>
   }
 
   /**
-   * makeQuery
+   * makeInsertQuery
    * @param queryType
    * @param datas
    * @param key
    * @param options
    * @returns string
    */
-  private makeQuery(
+  private makeInsertQuery(
     queryType: QueryType,
     datas: Array<T>,
     key: string | undefined = undefined,
@@ -156,6 +241,41 @@ export default class MysqlHandler<T extends IndexSignature>
         break;
       default:
     }
+
+    return queries.join(' ');
+  }
+
+  private makeSelectQuery(): string
+  {
+    const queries: Array<string> = [];
+
+    queries.push(`SELECT ${this.fields.length ? this.fields.map(this.backQuotes).join(', ') : '*'}`);
+    queries.push(`FROM \`${this.tableName}\``);
+    queries.push('WHERE');
+
+    if (!this.whereCondition.size) {
+      queries.push('1');
+    } else {
+      const conditions: string[] = [];
+
+      this.whereCondition.forEach((value: whereValueCondition, key: string) => {
+        switch (typeof value) {
+          case 'string':
+          case 'number':
+            conditions.push(`\`${key}\`=?`);
+            break;
+          case 'boolean':
+            conditions.push(`\`${key}\` IS ${value ? 'EXISTS' : 'NOT EXISTS'}`);
+            break;
+          default:
+        }
+      });
+
+      queries.push(conditions.join(' AND '));
+    }
+
+    // offset
+    queries.push(`LIMIT ${this._limit} OFFSET ${this._offset}`);
 
     return queries.join(' ');
   }
