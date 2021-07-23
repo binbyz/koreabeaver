502f29f23d4a0ed981e43f8d23841c006380869b
kingz <eyedroot@gmail.com>
2021-07-24 00:42:54 +0900

    update: main()

--- DataCollector/src/index.ts
+++ DataCollector/src/index.ts
@@ -2,8 +2,10 @@ import 'dotenv/config';
 
 import Mdcin from './classes/collector/Mdcin';
 import MdcinModel from './classes/models/MdcinModel';
+import CollectorHistory from './classes/models/CollectorHistory';
 
 import { logger } from '../config/winston';
+import { Collector } from './types';
 
 main();
 
@@ -11,10 +13,14 @@ async function main()
 {
   logger.info('Start DataCollector');
 
+  const historyInst = new CollectorHistory();
   const mdcinInst = new Mdcin(process.env.MDCIN_ENCODING_KEY!);
 
+  // 이전 히스토리 가져오기
+  historyInst.where('type', Collector.Types.DATA24_MDCIN);
+
   mdcinInst.setPageNo(1);
-  mdcinInst.setNumOfRows(20);
+  mdcinInst.setNumOfRows(100);
 
   const mdcinModel: MdcinModel = new MdcinModel();
 
