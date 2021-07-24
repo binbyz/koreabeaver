87be448a51279ac362a9d0ea436b3fb8bf48dc43
kingz <eyedroot@gmail.com>
2021-07-24 21:06:25 +0900

    fix: main

--- DataCollector/src/index.ts
+++ DataCollector/src/index.ts
@@ -6,27 +6,47 @@ import CollectorHistory from './classes/models/CollectorHistory';
 
 import { logger } from '../config/winston';
 import { Collector } from './types';
+import { getLocaleDatetime } from './lib/slim';
 
+logger.info('Start DataCollector');
 main();
 
-async function main()
+/**
+ * Main Function
+ */
+function main()
 {
-  logger.info('Start DataCollector');
+  const collector = new Mdcin(process.env.MDCIN_ENCODING_KEY!);
 
-  const historyInst = new CollectorHistory();
-  const mdcinInst = new Mdcin(process.env.MDCIN_ENCODING_KEY!);
-
-  // 이전 히스토리 가져오기
-  historyInst.where('type', Collector.Types.DATA24_MDCIN);
-
-  mdcinInst.setPageNo(1);
-  mdcinInst.setNumOfRows(100);
-
+  const historyModel = new CollectorHistory();
   const mdcinModel: MdcinModel = new MdcinModel();
 
-  await mdcinInst.call()
-    .then(response => {
-      mdcinModel.loadXML(response).handle();
-    })
-    .catch(error => mdcinModel.throws(error));
+  setInterval(async () => {
+    // 이전 히스토리 가져오기
+    const historyOne = await historyModel.where('type', Collector.Types.DATA24_MDCIN).orderBy('id', 'desc').first();
+    let pageNo = 1;
+
+    if (historyOne !== null) {
+      pageNo = historyOne?.extra_data?.last_page ? (historyOne.extra_data.last_page + 1) : 1;
+    }
+
+    collector.setPageNo(pageNo);
+    collector.setNumOfRows(100);
+
+    // 응답 API 처리
+    await collector.call()
+      .then(response => {
+        // 응답값 처리
+        mdcinModel.loadXML(response).handle();
+      })
+      .catch(error => mdcinModel.throws(error));
+
+    // 마지막 페이징 업데이트
+    historyModel.clear().where('type', Collector.Types.DATA24_MDCIN).update({
+      "extra_data": {
+        "last_page": pageNo,
+        "last_updated": getLocaleDatetime(),
+      },
+    });
+  }, 5000);
 }
