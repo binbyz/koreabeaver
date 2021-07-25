833daa5c6b216f86a622c11a3a2f8c76cb4cba19
kingz <eyedroot@gmail.com>
2021-07-25 17:27:20 +0900

    update: app

--- application/src/App.vue
+++ application/src/App.vue
@@ -1,30 +1,34 @@
-<template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+<template lang="pug">
+#app
+  AppHeader
 </template>
 
+<script lang="ts">
+import { defineComponent } from 'vue'
+import AppHeader from './components/AppHeader.vue'
+
+export default defineComponent({
+  components: {
+    AppHeader
+  }
+})
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+body {
+  background-color: rgb(238, 240, 241);
+  font-family: -apple-system, system-ui, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+  line-height: 24px;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  margin: 0;
+  padding: 0;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+#app {
+  display: flex;
+  flex-direction: column;
+  min-height: 900px;
+  width: 1200px;
+  margin: 0 auto;
 }
 </style>
---
+++ application/src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+header
+  h1.logo
+    a(href="./") 대한민국 <strong>의약품</strong> 행정처분 정보
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+
+})
+</script>
+
+<style lang="scss" scoped>
+header {
+  text-align: center;
+}
+</style>
