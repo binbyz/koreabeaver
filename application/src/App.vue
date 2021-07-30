0a5a1b47178b1a6123f38daa0a0037c8dd8671ec
kingz <eyedroot@gmail.com>
2021-07-30 21:58:02 +0900

    fix: app reload

--- application/src/App.vue
+++ application/src/App.vue
@@ -5,7 +5,7 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import AppHeader from './components/AppHeader.vue'
+import AppHeader from '@/components/AppHeader.vue'
 
 export default defineComponent({
   components: {
