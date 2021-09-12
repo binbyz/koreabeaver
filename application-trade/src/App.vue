2a58edb72f5b94d54152ef0965881ffbe6d3d920
kingz <eyedroot@gmail.com>
2021-09-12 18:29:24 +0900

    init 맛동산

--- application-trade/src/App.vue
+++ application-trade/src/App.vue
@@ -1,30 +1,26 @@
-<template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+<template lang="pug">
+#app
+  AppHeader
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+import { defineComponent } from 'vue'
+import AppHeader from '@/components/AppHeader.vue'
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+export default defineComponent({
+  components: {
+    AppHeader
   }
+})
+</script>
+
+<style lang="scss">
+@import '@/assets/scss/_app.scss';
+
+#app {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  margin: 0 auto;
 }
 </style>
---
+++ application-trade/src/components/AppHeader.vue
@@ -0,0 +1,5 @@
+<template lang="pug">
+header.app-header
+  h1.logo
+    a(href="./") 맛동산
+</template>
