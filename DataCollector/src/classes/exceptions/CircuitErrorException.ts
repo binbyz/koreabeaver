d73c5f98f2ebcf8d80885acbd18b62d84043701c
kingz <eyedroot@gmail.com>
2021-09-12 17:38:12 +0900

    reinit

--- DataCollector/src/classes/exceptions/CircuitErrorException.ts
+++
@@ -1,7 +0,0 @@
-export default class CircuitErrorException extends Error
-{
-  public constructor(message: string)
-  {
-    super(message);
-  }
-}
--- DataCollector/test/next_month.ts
+++
@@ -1,9 +0,0 @@
-import moment from "moment";
-
-// console.log(
-//   moment('201501').add(1, 'months').format('YYYYMM')
-// );
-
-console.log(
-  moment().format('YYYY-MM-DD HH:mm:ss')
-)
