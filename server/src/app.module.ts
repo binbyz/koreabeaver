b9ccb829892e2ee3629a7f88ed18aac963239a78
kingz <eyedroot@gmail.com>
2021-10-30 16:23:42 +0900

    update: init for restapi

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,30 +1,9 @@
-import {
-  MiddlewareConsumer,
-  Module,
-  NestModule,
-  RequestMethod,
-} from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TestController } from './test/test.controller';
-import { CatsController } from './cats/cats.controller';
-import { CatsService } from './cats/cats.service';
-import { CatsModule } from './cats/cats.module';
-import { LoggerMiddleware } from './middlewares/logger.middleware';
-
 @Module({
-  imports: [CatsModule],
-  controllers: [AppController, CatsController, TestController],
-  providers: [AppService, CatsService],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(LoggerMiddleware)
-      .exclude(
-        { path: 'cats', method: RequestMethod.GET },
-        { path: 'cats', method: RequestMethod.POST },
-      )
-      .forRoutes(CatsController);
-  }
-}
+export class AppModule {}
